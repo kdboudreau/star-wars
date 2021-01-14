fd09df5cb0a2986905b081a91a7c27f9c464b58a
Kathryn Boudreau <kdboudreau1@gmail.com>
2021-01-14 16:54:33 -0400

    adding components and tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
